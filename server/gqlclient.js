e37bb6173fe7dd149058c715960b2201d965e396
Howard Tang <howard@tray.io>
2018-07-11 14:48:28 +0100

    Add options that turn off heavy in mem caching

--- server/gqlclient.js
+++ server/gqlclient.js
@@ -7,6 +7,17 @@ import {InMemoryCache} from 'apollo-cache-inmemory';
 const gqlEndpoint = 'https://54srzzin5j.execute-api.eu-west-1.amazonaws.com/staging/graphql';
 const masterToken = process.env.MASTER_TOKEN;
 
+const defaultOptions = {
+      watchQuery: {
+        fetchPolicy: 'network-only',
+        errorPolicy: 'ignore',
+      },
+      query: {
+        fetchPolicy: 'network-only',
+        errorPolicy: 'all',
+      },
+    }
+
 // Create a Apollo Client Context for a given auth token:
 const authLink = token =>
     setContext((_, {headers}) => ({
@@ -21,6 +32,7 @@ const generateClient = token =>
     new ApolloClient({
         link: authLink(token).concat(new HttpLink({uri: gqlEndpoint, fetch})),
         cache: new InMemoryCache(),
+        defaultOptions,
     });
 
 module.exports = {
