14371b1e70e8f402f2886b34c1e39fa67d82d28e
stoich <stoil@tray.io>
2018-08-04 13:28:26 +0100

    env.ENDPOINT is now also used to setup app url

--- server/configuration.js
+++ server/configuration.js
@@ -2,42 +2,38 @@ export const setEnvironment = () => {
     const productionGraphqlEndpoint = 'https://tray.io/graphql';
     const stagingGraphqlEndpoint = 'https://staging.tray.io/graphql';
 
+    const appUrlStaging = 'https://app-staging.tray.io';
+    const appUrlProd = 'https://app.tray.io';
+
     switch (process.env.ENDPOINT) {
         case undefined:
             console.log(`ENDPOINT was not passed. Defaulting to Prod ${productionGraphqlEndpoint}`);
             process.env.ENDPOINT = productionGraphqlEndpoint;
+            process.env.APP_URL = productionGraphqlEndpoint;
             break;
         case 'stg':
             console.log(`ENDPOINT passed as Staging`);
             process.env.ENDPOINT = stagingGraphqlEndpoint
+            process.env.APP_URL = appUrlStaging;
             break;
         case 'staging':
             console.log(`ENDPOINT passed as Staging`);
             process.env.ENDPOINT = stagingGraphqlEndpoint
+            process.env.APP_URL = appUrlStaging;
             break;
         case 'prod':
             console.log(`ENDPOINT passed as Production`);
             process.env.ENDPOINT = productionGraphqlEndpoint
+            process.env.APP_URL = appUrlProd;
             break;
         case 'production':
             console.log(`ENDPOINT passed as Production`);
             process.env.ENDPOINT = productionGraphqlEndpoint
+            process.env.APP_URL = appUrlProd;
             break;
         default:
-            if (isValidUrl(process.env.ENDPOINT)) {
-                console.log(`Using custom Graphql endpoint ${process.env.ENDPOINT}`)
-            }
-            else {
-                throw new Error(`The defined values for Graphql ${process.env.ENDPOINT} is not a valid URL or alias.`);
-            }
+            throw new Error(`The defined values for Graphql ${process.env.ENDPOINT} is not a valid URL alias (use 'stg' or 'prod')`);
             break;
     }
 
 }
-
-const isValidUrl = url => {
-    var urlRegex = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
-    var regex = new RegExp(urlRegex);
-    return url.match(regex);
-}
-
