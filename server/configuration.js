4e9174fd74f2aa1cce3c6be61bc0f0f952646444
Howard Tang <howard@tray.io>
2018-08-09 15:05:17 +0100

    Refactor and fix bug in configuration

--- server/configuration.js
+++ server/configuration.js
@@ -6,34 +6,22 @@ export const setEnvironment = () => {
     const appUrlProd = 'https://app.tray.io';
 
     switch (process.env.ENDPOINT) {
-        case undefined:
-            console.log(`ENDPOINT was not passed. Defaulting to Prod ${productionGraphqlEndpoint}`);
-            process.env.ENDPOINT = productionGraphqlEndpoint;
-            process.env.APP_URL = productionGraphqlEndpoint;
-            break;
         case 'stg':
-            console.log(`ENDPOINT passed as Staging`);
-            process.env.ENDPOINT = stagingGraphqlEndpoint
-            process.env.APP_URL = appUrlStaging;
-            break;
         case 'staging':
             console.log(`ENDPOINT passed as Staging`);
-            process.env.ENDPOINT = stagingGraphqlEndpoint
+            process.env.ENDPOINT = stagingGraphqlEndpoint;
             process.env.APP_URL = appUrlStaging;
             break;
         case 'prod':
-            console.log(`ENDPOINT passed as Production`);
-            process.env.ENDPOINT = productionGraphqlEndpoint
-            process.env.APP_URL = appUrlProd;
-            break;
         case 'production':
             console.log(`ENDPOINT passed as Production`);
-            process.env.ENDPOINT = productionGraphqlEndpoint
+            process.env.ENDPOINT = productionGraphqlEndpoint;
             process.env.APP_URL = appUrlProd;
             break;
         default:
-            throw new Error(`The defined values for Graphql ${process.env.ENDPOINT} is not a valid URL alias (use 'stg' or 'prod')`);
+            console.log(`No valid ENDPOINT was passed. Defaulting to Prod ${productionGraphqlEndpoint}`);
+            process.env.ENDPOINT = productionGraphqlEndpoint;
+            process.env.APP_URL = appUrlProd;
             break;
     }
-
 }
