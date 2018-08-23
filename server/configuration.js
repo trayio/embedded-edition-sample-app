f7ab8ac923d4011463323c60f9880e5b54785a94
stoich <stoil@tray.io>
2018-08-23 19:34:13 +0100

    Fix endpoint

--- server/configuration.js
+++ server/configuration.js
@@ -10,18 +10,18 @@ export const setEnvironment = () => {
         case 'staging':
             console.log(`ENDPOINT passed as Staging`);
             process.env.TRAY_ENDPOINT = stagingGraphqlEndpoint;
-            process.env.APP_URL = appUrlStaging;
+            process.env.TRAY_APP_URL = appUrlStaging;
             break;
         case 'prod':
         case 'production':
             console.log(`ENDPOINT passed as Production`);
             process.env.TRAY_ENDPOINT = productionGraphqlEndpoint;
-            process.env.APP_URL = appUrlProd;
+            process.env.TRAY_APP_URL = appUrlProd;
             break;
         default:
             console.log(`No valid ENDPOINT was passed. Defaulting to Prod ${productionGraphqlEndpoint}`);
             process.env.TRAY_ENDPOINT = productionGraphqlEndpoint;
-            process.env.APP_URL = appUrlProd;
+            process.env.TRAY_APP_URL = appUrlProd;
             break;
     }
 }
