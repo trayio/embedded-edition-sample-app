778cafe717e86522813bb5365c3281ddf4b150a0
Howard Tang <howard@tray.io>
2018-09-19 16:49:29 +0100

    Fix setup script

--- setup.sh
+++ setup.sh
@@ -3,12 +3,14 @@ read -p "Enter TRAY_ENDPOINT - prod or stg (prod): " ENDPOINT
 ENDPOINT=${ENDPOINT:-prod}
 
 read -p "Enter TRAY_PARTNER - name of partner, used for custom CSS + external route name (asana): " PARTNER
-PARTNER=${x:-asana}
+PARTNER=${PARTNER:-asana}
 
 read -p "Enter TRAY_MASTER_TOKEN - the master token for your partner account (""): " MASTER_TOKEN
-PARTNER=${x:-asana}
+if [ -e ".token" ]; then
+    MASTER_TOKEN=${MASTER_TOKEN:-$(cat .token)}
+fi
 
-if [ -z "$MASTER_TOKEN"]; then
+if [ -z "$MASTER_TOKEN" ]; then
     echo ""
     echo "Error: Please run the script with your partner account master token"
     echo ""
@@ -19,8 +21,11 @@ else
     export TRAY_PARTNER=$PARTNER
     export TRAY_MASTER_TOKEN=$MASTER_TOKEN
 
+    > .token;
+    echo "$MASTER_TOKEN" >> .token
+
     # Run api and app:
-    kill $(lsof -i:3001 -t) 2> /dev/null || sudo kill -9 $(lsof -i:3001 -t) 2> /dev/null
-    kill $(lsof -i:3002 -t) 2> /dev/null || sudo kill -9 $(lsof -i:3002 -t) 2> /dev/null
+    sudo kill -9 $(lsof -i:3000 -t);
+    sudo kill -9 $(lsof -i:3001 -t);
     yarn run api & yarn run start
 fi
