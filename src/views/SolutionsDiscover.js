25a1e1616f0d1bdca94e7b938ad386353bf3c469
Jose Rego <jose@tray.io>
2018-11-13 10:09:19 +0000

    listen to solution instance pop up events

--- src/views/SolutionsDiscover.js
+++ src/views/SolutionsDiscover.js
@@ -47,6 +47,39 @@ export class DiscoverIntegrations extends React.PureComponent {
             'width=500,height=500,scrollbars=no',
         );
 
+        // Listen to popup messages
+        let configFinished = false;
+        const onmessage = e => {
+            if (e.data.type === 'tray.configPopup.error') {
+                // Handle popup error message
+                alert(`Error: ${e.data.err}`);
+            }
+            if (e.data.type === 'tray.configPopup.finish') {
+                // Handle popup finish message
+                configFinished = true;
+            }
+        }
+        window.addEventListener('message', onmessage);
+
+        // Check if popup window has been closed before finishing the configuration.
+        // We use a polling function due to the fact that some browsers may not
+        // display prompts created in the beforeunload event handler.
+        const CHECK_TIMEOUT = 1000;
+        const checkWindow = () => {
+            if (configWindow.closed) {
+                // Handle popup closing
+                if (!configFinished) {
+                    alert('Configuration not finished');
+                } else {
+                    console.log('Configuration finished');
+                }
+                window.removeEventListener('message', onmessage);
+            } else {
+                setTimeout(checkWindow, CHECK_TIMEOUT);
+            }
+        }
+        checkWindow();
+
         createSolutionInstance(id, name).then(({body}) => {
             // After we generate the popup URL, set it to the previously opened
             // window:
