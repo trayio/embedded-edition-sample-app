ee52b38e0e2191fd688579483da97df9ab790a35
jdon <jonathan@tray.io>
2019-06-04 15:15:02 +0100

    Revert "Added validation for the setup page"
    This reverts commit ffb3f53b7179f14dc5c7d7db71d219c3dcd1a72c.

--- src/components/auth/Setup.js
+++ src/components/auth/Setup.js
@@ -1,58 +1,43 @@
 import React from 'react';
-import SetupForm from './SetupForm';
-import Loading from '../Loading';
+import SetupForm from './SetupForm'
+import Loading from "../Loading";
 import Cookies from 'js-cookie';
 
-
 export default class Login extends React.Component {
-	state = {
-		loading: false,
-		redirectToReferrer: false,
-	};
+    state = {
+        loading: false,
+        redirectToReferrer: false
+    }
 
-	showError = errorMessage =>
-		this.setState({
-			error: true,
-			loading: false,
-			message: errorMessage,
-		});
+    setup = (data) => {
+        console.log('Setting up with ' + data.token);
+        Cookies.set('master_token', data.token);
+        Cookies.set('partner_name', data.partner);
 
-	setup = data => {
-		if (!data.token) {
-			this.showError('Master token is required!');
-		} else {
-			Cookies.set('master_token', data.token);
-			Cookies.set('partner_name', data.partner);
+        window.location.replace('/login');
+    }
 
-			window.location.replace('/login');
-		}
-	};
+    render() {
+        if (Cookies.get('master_token') ){
+            window.location.replace('/login')
+        }
 
-	render() {
-		if (Cookies.get('master_token')) {
-			window.location.replace('/login');
-		}
+        const style = {
+            container: {
+                height: "300px",
+            },
+            warning: {
+                textAlign: "center",
+                border: "none",
+            },
+        };
 
-		const style = {
-			container: {
-				height: '300px',
-			},
-			warning: {
-				textAlign: 'center',
-				border: 'none',
-			},
-		};
-
-		return (
-			<div style={style.container}>
-				<Loading loading={this.state.loading}>
-					<SetupForm
-						onSetup={this.setup}
-						error={this.state.error}
-						message={this.state.message}
-					/>
-				</Loading>
-			</div>
-		);
-	}
+        return (
+            <div style={style.container}>
+                <Loading loading={this.state.loading}>
+                    <SetupForm onSetup={this.setup}/>
+                </Loading>
+            </div>
+        );
+    }
 }
