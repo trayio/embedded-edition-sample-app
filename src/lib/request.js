5ab4887f8a9dcc1ab1bd5020adffdd4b6d6bc3b7
Andrew Jones <andrew@tray.io>
2019-06-04 12:03:56 +0100

    Add css selection to setup screen:

--- src/lib/request.js
+++ src/lib/request.js
@@ -1,14 +1,14 @@
 import Cookies from 'js-cookie';
 
-export const request = (path, options) =>
+export const request = (path, options, sendHeaders) =>
     fetch(path, {
         ...options,
-        headers: {
+        headers: sendHeaders ? {
             user_uuid: Cookies.get('user_uuid'),
             access_token: Cookies.get('access_token'),
             master_token: Cookies.get('master_token'),
             partner_name: Cookies.get('partner_name'),
             tray_id: Cookies.get('tray_id'),
             ...options.headers,
-        }
+        } : options.headers
     });
